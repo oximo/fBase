dbdf8716641e87da5d46e5bb3b414ce8d56455c8
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2021-10-23 23:35:51 +0100

    Add files via upload

---
+++ resources/[ 5.Jobs ]/fCore/server/[ fSplit ]/sv_split.lua
@@ -0,0 +1,193 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+TriggerEvent('esx_phone:registerNumber', 'split', 'alerte split', true, true)
+
+TriggerEvent('esx_society:registerSociety', 'split', 'split', 'society_split', 'society_split', 'society_split', 'society_split_black', {type = 'public'})
+
+ESX.RegisterServerCallback('fsplit:getStockItems', function(source, cb)
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_split', function(inventory)
+		cb(inventory.items)
+	end)
+end)
+
+RegisterNetEvent('fsplit:getStockItem')
+AddEventHandler('fsplit:getStockItem', function(itemName, count)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_split', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- is there enough in the society?
+		if count > 0 and inventoryItem.count >= count then
+				inventory.removeItem(itemName, count)
+				xPlayer.addInventoryItem(itemName, count)
+				TriggerClientEvent('esx:showNotification', _source, 'Objet retiré', count, inventoryItem.label)
+		else
+			TriggerClientEvent('esx:showNotification', _source, "Quantité invalide")
+		end
+	end)
+end)
+
+ESX.RegisterServerCallback('fsplit:getPlayerInventory', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local items   = xPlayer.inventory
+
+	cb({items = items})
+end)
+
+RegisterNetEvent('fsplit:putStockItems')
+AddEventHandler('fsplit:putStockItems', function(itemName, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_split', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- does the player have enough of the item?
+		if sourceItem.count >= count and count > 0 then
+			xPlayer.removeInventoryItem(itemName, count)
+			inventory.addItem(itemName, count)
+			xPlayer.showNotification(_U('have_deposited', count, inventoryItem.name))
+		else
+			TriggerClientEvent('esx:showNotification', _source, "Quantité invalide")
+		end
+	end)
+end)
+
+
+
+
+RegisterNetEvent('fSplit:bar')
+AddEventHandler('fSplit:bar', function(item,price)
+
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local xMoney = xPlayer.getMoney()
+
+    if xMoney >= price then
+        xPlayer.removeMoney(price)
+        xPlayer.addInventoryItem(item, 1)
+        TriggerClientEvent('esx:showNotification', source, "~g~Achats~w~ effectué !")
+    else
+         TriggerClientEvent('esx:showNotification', source, "Vous n'avez assez ~r~d\'argent")
+    end
+end)
+
+
+RegisterServerEvent('fSplit:Ouvert')
+AddEventHandler('fSplit:Ouvert', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local xPlayers	= ESX.GetPlayers()
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Split Sides', '~g~Informations', 'Le Split Sides est ouvert', 'CHAR_PROPERTY_BAR_UNBRANDED', 2)
+	end
+end)
+
+RegisterServerEvent('fSplit:Fermer')
+AddEventHandler('fSplit:Fermer', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local xPlayers	= ESX.GetPlayers()
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Split Sides', '~g~Informations', 'Le Split est fermé', 'CHAR_PROPERTY_BAR_UNBRANDED', 2)
+	end
+end)
+
+RegisterServerEvent('fSplit:Soire')
+AddEventHandler('fSplit:Soire', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local xPlayers	= ESX.GetPlayers()
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Split Sides', '~g~Informations', 'La fête commence rendez-vous au Split Sides', 'CHAR_PROPERTY_BAR_UNBRANDED', 2)
+	end
+end)
+
+
+RegisterServerEvent('fSplit:Perso')
+AddEventHandler('fSplit:Perso', function(msg)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local xPlayers    = ESX.GetPlayers()
+    for i=1, #xPlayers, 1 do
+        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+        TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Split Sides', '~g~Annonce', msg, 'CHAR_PROPERTY_BAR_UNBRANDED', 8)
+    end
+end)
+
+ESX.RegisterServerCallback('split:getPlayerInventoryBlack', function(source, cb)
+	local _source = source
+	local xPlayer    = ESX.GetPlayerFromId(_source)
+	local blackMoney = xPlayer.getAccount('black_money').money
+  
+	cb({
+	  blackMoney = blackMoney
+	})
+  end)
+
+RegisterServerEvent('split:putblackmoney')
+AddEventHandler('split:putblackmoney', function(type, item, count)
+
+  local _source      = source
+  local xPlayer      = ESX.GetPlayerFromId(_source)
+
+  if type == 'item_account' then
+    local playerAccountMoney = xPlayer.getAccount(item).money
+
+    if playerAccountMoney >= count then
+
+      xPlayer.removeAccountMoney(item, count)
+      TriggerEvent('esx_addonaccount:getSharedAccount', 'society_split_black', function(account)
+        account.addMoney(count)
+      end)
+    else
+      TriggerClientEvent('esx:showNotification', _source, 'Montant invalide')
+    end
+  end
+end)
+
+
+  ESX.RegisterServerCallback('split:getBlackMoneySociety', function(source, cb)
+    local _source = source
+    local xPlayer    = ESX.GetPlayerFromId(_source)
+    local blackMoney = 0
+  
+    TriggerEvent('esx_addonaccount:getSharedAccount', 'society_split_black', function(account)
+      blackMoney = account.money
+    end)
+  
+    cb({
+      blackMoney = blackMoney
+    })
+  
+  end)
+
+  RegisterServerEvent('split:getItem')
+  AddEventHandler('split:getItem', function(type, item, count)
+  
+    local _source      = source
+    local xPlayer      = ESX.GetPlayerFromId(_source)
+  
+    if type == 'item_account' then
+  
+      TriggerEvent('esx_addonaccount:getSharedAccount', 'society_split_black', function(account)
+  
+        local roomAccountMoney = account.money
+  
+        if roomAccountMoney >= count then
+          account.removeMoney(count)
+          xPlayer.addAccountMoney(item, count)
+        else
+          TriggerClientEvent('esx:showNotification', _source, 'Montant invalide')
+        end
+  
+      end)
+    end
+end)
