bf1a8049b9a89dbfe02eca13156843fd030f446d
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2021-11-16 09:38:58 +0000

    Add files via upload

--- resources/[ 3.ESX ]/esx_basicneeds/server/main.lua
+++ resources/[ 3.ESX ]/esx_basicneeds/server/main.lua
@@ -16,6 +16,51 @@ ESX.RegisterUsableItem('water', function(source)
 	xPlayer.showNotification(_U('used_water'))
 end)
 
+ESX.RegisterUsableItem('doublecheese', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('doublecheese', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 350000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	xPlayer.showNotification(_U('used_doublecheese'))
+end)
+
+ESX.RegisterUsableItem('dvingtnuggets', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('dvingtnuggets', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 250000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	xPlayer.showNotification(_U('used_vingtnuggets'))
+end)
+
+ESX.RegisterUsableItem('potatoes', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('potatoes', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 280000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	xPlayer.showNotification(_U('used_potatoes'))
+end)
+
+ESX.RegisterUsableItem('frites', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('frites', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 220000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	xPlayer.showNotification(_U('used_frites'))
+end)
+
+ESX.RegisterUsableItem('ketchup', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('ketchup', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 50000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	xPlayer.showNotification(_U('used_ketchup'))
+end)
+
 ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
 	args.playerId.triggerEvent('esx_basicneeds:healPlayer')
 	args.playerId.triggerEvent('chat:addMessage', {args = {'^5HEAL', 'You have been healed.'}})
